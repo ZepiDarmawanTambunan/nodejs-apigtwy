67d77217076ba293f06375a44ddb7c91ab98eba9
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-06-26 19:37:01 +0700

    integrate to service_media

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const coursesRouter = require('./routes/courses');
+const indexRouter = require('./routes/index');
+const mediaRouter = require('./routes/media');
+const ordersRouter = require('./routes/orders');
+const paymentsRouter = require('./routes/payments');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'})); //limit krn base64 string yg panjang
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/courses', coursesRouter);
+app.use('/media', mediaRouter);
+app.use('/orders', ordersRouter);
+app.use('/payments', paymentsRouter);
 
 module.exports = app;
--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {APP_NAME} = process.env;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log(APP_NAME);
-  res.send(APP_NAME);
+  res.send('courses');
 });
 
 module.exports = router;
